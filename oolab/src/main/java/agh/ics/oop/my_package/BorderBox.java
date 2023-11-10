ae79fc70db6938fbed7bdc739b654c37ee5e0357
barszu from pc <bartek.szu@op.pl>
2023-11-10 10:52:13 +0100

    poprawiono BorderBox

--- oolab/src/main/java/agh/ics/oop/my_package/BorderBox.java
+++ oolab/src/main/java/agh/ics/oop/my_package/BorderBox.java
@@ -8,25 +8,28 @@ public class BorderBox {
     //designating a field just like selecting a part of the screen by mouse
     private final Vector2d startPoint; //left,up most point
     private final Vector2d endPoint; //right,down most point
+    private final Vector2d borderLeftDown;
+    private final Vector2d borderRightUp;
 
-    public static final int WORLD_MAP_SIZE = 4; //GLOBAL SETTING, can be changed
+    public static final int WORLD_MAP_SIZE = 5; //GLOBAL SETTING, can be changed
 
     public BorderBox(){
         this.startPoint = new Vector2d(0,0);
-        this.endPoint = new Vector2d(WORLD_MAP_SIZE , WORLD_MAP_SIZE);
+        this.endPoint = new Vector2d(WORLD_MAP_SIZE-1 , WORLD_MAP_SIZE-1);
+        this.borderLeftDown = this.startPoint.lowerLeft(this.endPoint);
+        this.borderRightUp = this.startPoint.upperRight(this.endPoint);
     }
 
     public BorderBox(Vector2d startPoint , Vector2d endPoint) {
         this.startPoint = startPoint;
         this.endPoint = endPoint;
+        this.borderLeftDown = this.startPoint.lowerLeft(this.endPoint);
+        this.borderRightUp = this.startPoint.upperRight(this.endPoint);
     }
 
     public boolean contains(Vector2d point){
         // if point is Null it will crash on Vector2d
         // variables for easy code
-        Vector2d borderLeftDown = this.startPoint.lowerLeft(this.endPoint);
-        Vector2d borderRightUp = this.startPoint.upperRight(this.endPoint);
-
-        return point.precedes(borderRightUp) && point.follows(borderLeftDown);
+        return point.precedes(this.borderRightUp) && point.follows(this.borderLeftDown);
     }
 }
